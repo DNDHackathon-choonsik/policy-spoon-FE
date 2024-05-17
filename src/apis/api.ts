3ded6251a3bbdb98a6383604387b2bb6a2990baf
Junhyeong Park <pljh0906@yonsei.ac.kr>
2024-05-18 08:33:04 +0900

    feat: review api set complete

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,8 +1,11 @@
+import LocalStorage from "@/utils/localStorage";
 import axios from "axios";
 
 interface ChatBotResponse {
   answer: string;
 }
+
+const ACCESS_TOKEN = LocalStorage.getItem("accessToken");
 
 export const getChatBot = async (question: string): Promise<string> => {
   try {
@@ -21,4 +24,15 @@ export const getChatBot = async (question: string): Promise<string> => {
     console.error("Error in getChatBot:", error);
     throw new Error("챗봇 응답을 가져오는 중 오류가 발생했습니다.");
   }
+};
+
+export const getReview = async () => {
+  const response = await axios.get(`http://192.168.10.116:4321/review/1`, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      "Access-Control-Allow-Origin": "*",
+      Authorization: `Bearer ` + ACCESS_TOKEN,
+    },
+  });
+  return response.data.data;
 };
