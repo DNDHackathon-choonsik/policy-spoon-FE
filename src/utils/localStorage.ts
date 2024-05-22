1de86b4ba55c62f6534c50db437a33a7f5d97d02
leedahye <leedahye2001@naver.com>
2024-05-22 17:13:47 +0900

    fix: localStorage is not defined

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,20 +1,21 @@
 class LocalStorage {
-  constructor() {
-    if (typeof window === "undefined") {
-      throw new Error("LocalStorage cannot be used in server-side rendering.");
+  constructor() {}
+  static setItem(key: string, value: string) {
+    if (typeof window !== "undefined") {
+      localStorage.setItem(key, value);
     }
   }
-
-  static setItem(key: string, value: string) {
-    localStorage.setItem(key, value);
-  }
-
   static getItem(key: string) {
-    return localStorage.getItem(key);
+    if (typeof window !== "undefined") {
+      return localStorage.getItem(key);
+    }
+    return null;
   }
 
   static removeItem(key: string) {
-    localStorage.removeItem(key);
+    if (typeof window !== "undefined") {
+      localStorage.removeItem(key);
+    }
   }
 }
 
